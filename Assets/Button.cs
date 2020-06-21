08f896a5be3c4147fc5c5864a2a0666834f918b1
”KA-gamecreate” <burakki.efi.22@docomo.ne.jp>
2020-06-21 14:06:31 +0900

    “最後まで終わった”

---
+++ Assets/Button.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Main1()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void Main2()
+    {
+        SceneManager.LoadScene("main2");
+    }
+    public void Main3()
+    {
+        SceneManager.LoadScene("main3");
+    }
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    float speed = 10.0f;
+    public GameObject gameOverCanbas;
+    bool gameOver = false;
+
+    public GameObject gameClearCanbas;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+            float y = Input.GetAxis("Vertical");
+            float x = Input.GetAxis("Horizontal");
+
+            Vector3 velocity = GetComponent<Rigidbody>().velocity;
+            velocity.x = x * 10;
+            velocity.y = y * 10;
+            GetComponent<Rigidbody>().velocity = velocity;
+
+            transform.rotation = Quaternion.AngleAxis(x * -30, Vector3.forward);
+        }
+            if (transform.position.z >= 150)
+            {
+            gameClearCanbas.SetActive(true);
+            }
+        
+    }
+    void GameOver()
+    {
+        gameOver = true;
+        gameOverCanbas.SetActive(true);
+    }
+}
